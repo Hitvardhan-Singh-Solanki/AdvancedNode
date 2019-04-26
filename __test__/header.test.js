fb681ec372bbbb70e0d7534e720dbdbd3b9a08e4
hitvardhansolanki <43982595+hitvardhasolanki@users.noreply.github.com>
2019-04-27 04:17:22 +0530

    running chromium from test suite

--- __test__/header.test.js
+++ __test__/header.test.js
@@ -1,4 +1,14 @@
+const puppeteer = require('puppeteer');
+
 test('test add two numbers', () => {
   const sum = 1 + 2;
   expect(sum).toEqual(3);
+});
+
+test('launch browser', async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto('http://localhost:3000');
+  const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+  expect(text).toEqual('Blogster');
 });
