2dc5fc2ccc1694b8fee07ca1650d60a7aeddb60d
hitvardhansolanki <43982595+hitvardhasolanki@users.noreply.github.com>
2019-04-27 04:45:36 +0530

    added test to start the OAuth flow

--- __test__/header.test.js
+++ __test__/header.test.js
@@ -1,14 +1,25 @@
 const puppeteer = require('puppeteer');
 
-test('test add two numbers', () => {
-  const sum = 1 + 2;
-  expect(sum).toEqual(3);
+let browser, page;
+
+beforeEach(async () => {
+  browser = await puppeteer.launch({ headless: false });
+  page = await browser.newPage();
+  await page.goto('http://localhost:3000');
 });
 
-test('launch browser', async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto('http://localhost:3000');
+afterEach(async () => {
+  await browser.close();
+});
+
+test('header has the correct text', async () => {
   const text = await page.$eval('a.brand-logo', el => el.innerHTML);
   expect(text).toEqual('Blogster');
+});
+
+test('start OAuth flow when click login', async () => {
+  console.log('test');
+  await page.click('ul.right a');
+  const url = await page.url();
+  expect(url).toMatch(/accounts\.google\.com/);
 });
