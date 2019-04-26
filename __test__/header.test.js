719aa54c54a0ec348c783b4ca7cfc1548ae653e6
hitvardhasolanki <43982595+hitvardhasolanki@users.noreply.github.com>
2019-04-27 04:01:27 +0530

    first commit

---
+++ __test__/header.test.js
@@ -0,0 +1,4 @@
+test('test add two numbers', () => {
+  const sum = 1 + 2;
+  expect(sum).toEqual(3);
+});
---
+++ services/cache.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
+
+const exec = mongoose.Query.prototype.exec;
+const redisUrl = 'redis://127.0.0.1:6379';
+
+const client = redis.createClient(redisUrl);
+client.hget = util.promisify(client.hget);
+
+mongoose.Query.prototype.cache = function(options = {}) {
+  this.useCache = true;
+  this.haskey = JSON.stringify(options.key || '');
+  return this;
+};
+
+mongoose.Query.prototype.exec = async function() {
+  if (!this.useCache) {
+    return exec.apply(this, arguments);
+  }
+  const key = JSON.stringify({
+    ...this.getQuery(),
+    collection: this.mongooseCollection.name
+  });
+
+  const cacheValue = await client.hget(this.haskey, key);
+
+  if (cacheValue) {
+    doc = JSON.parse(cacheValue);
+    return Array.isArray(doc)
+      ? doc.map(ele => new this.model(ele))
+      : new this.model(doc);
+  }
+
+  const result = await exec.apply(this, arguments);
+
+  client.hset(this.haskey, key, JSON.stringify(result));
+
+  return result;
+};
+
+module.exports = {
+  clearHash(hashkey) {
+    client.del(JSON.stringify(hashkey));
+  }
+};
